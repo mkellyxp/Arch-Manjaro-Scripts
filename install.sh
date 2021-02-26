38879f8c14f39e711d868144fdeffbea8643578a
Mike Kelly <mike@membervault.co>
2021-02-26 08:28:29 -0800

    Update for PHP8

--- install.sh
+++ install.sh
@@ -16,9 +16,9 @@ else
     sudo pacman -S apache php php-gd php-apache mysql
 
     echo 'LoadModule mpm_prefork_module modules/mod_mpm_prefork.so' | sudo tee -a /etc/httpd/conf/httpd.conf
-    echo 'LoadModule php7_module modules/libphp7.so' | sudo tee -a /etc/httpd/conf/httpd.conf
-    echo 'AddHandler php7-script php' | sudo tee -a /etc/httpd/conf/httpd.conf
-    echo 'Include conf/extra/php7_module.conf' | sudo tee -a /etc/httpd/conf/httpd.conf
+    echo 'LoadModule php_module modules/libphp.so' | sudo tee -a /etc/httpd/conf/httpd.conf
+    echo 'AddHandler php-script php' | sudo tee -a /etc/httpd/conf/httpd.conf
+    echo 'Include conf/extra/php_module.conf' | sudo tee -a /etc/httpd/conf/httpd.conf
 
     sudo mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 
@@ -63,12 +63,14 @@ else
     echo "*                                                              *"
     echo "* - Visit 'http://localhost/info.php' to confirm PHP is set up *"
     echo "* - Your document root is set to: '/srv/http/'                 *"
-    echo "* - Your apache config is at: '/etc/httpd/'                    *"
-    echo "* - Your php config is at: '/etc/php/7.x/apache2/'             *"
+    echo "* - Your apache config is at: '/etc/httpd/conf'                *"
+    echo "* - Your php config is at: '/etc/php/php.ini'                  *"
     echo "* - Log into mysql with: 'mysql -u root -p'                    *"
     echo "*                                                              *"
     echo "* - Restart apache: 'sudo systemctl restart httpd'             *"
     echo "* - Restart mysql: 'sudo service mysql restart'                *"
     echo "*                                                              *"
     echo "****************************************************************"
+    echo ""
+    echo "NOTE: Please comment out the 'LoadModule mpm_event_module modules/mod_mpm_event.so' line in /etc/httpd/conf/httpd.conf "
 fi
