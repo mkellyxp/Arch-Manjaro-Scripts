b1eae98f3de928e37aacac49504bff457e3f3472
Mike Kelly <mike@membervault.co>
2021-02-27 08:07:10 -0800

    Set up Plasma Environment

---
+++ install_plasma_dev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo pacman -Syyuu
+
+# Install base plasma stuff
+sudo pacman -S xorg plasma kde-graphics sddm konsole dolphin firefox gnome-keyring
+sudo systemctl enable sddm
+
+# Install coding and personal apps
+sudo pacman -S code telegram-desktop neovim git dbeaver
+yay -S gitkraken
+yay -S mailspring
+yay -S spotify
