c0b94d16311ee28dede7b795312c39e317908491
Mike Kelly <mike@membervault.co>
2021-03-06 12:22:50 -0800

    added

--- install_plasma_dev.sh
+++ install_plasma_dev.sh
@@ -3,12 +3,12 @@
 sudo pacman -Syyuu
 
 # Install base plasma stuff
-sudo pacman -S xorg plasma kde-graphics sddm konsole dolphin firefox gnome-keyring mintstick
+sudo pacman -S xorg plasma kde-graphics sddm konsole dolphin firefox gnome-keyring mintstick unzip
 sudo pacman -Rs kimagemapeditor
 sudo systemctl enable sddm
 
 # Install coding and personal apps
-sudo pacman -S code telegram-desktop neovim git dbeaver
+sudo pacman -S code telegram-desktop neovim git dbeaver elixir nodejs npm
 yay -S gitkraken
 yay -S mailspring
 yay -S spotify
