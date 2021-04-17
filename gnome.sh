22fedf2f0b776ef45d62b68be8e74bbd12dfa9ff
Mike Kelly <mike@membervault.co>
2021-04-17 08:21:29 -0700

    gnome 40 updates

--- gnome.sh
+++ gnome.sh
@@ -3,11 +3,18 @@
 sudo pacman -Syyuu
 
 # Install base gnome stuff
-sudo pacman -S xorg gnome gnome-terminal gnome-calendar gnome-tweaks gnome-control-center firefox gdm
+sudo pacman -S base-devel xorg gnome gnome-tweaks firefox gdm libappindicator-gtk3 flatpak arc-icon-theme ttf-roboto arc-gtk-theme chrome-gnome-shell
 sudo systemctl enable gdm
+
+# Install yay
+cd /opt/
+sudo git clone https://aur.archlinux.org/yay-git.git
+sudo chown -R mkelly:mkelly ./yay-git
+cd yay-git/
+makepkg -si
+yay -Syyuu
 
 # Install coding and personal apps
 sudo pacman -S code telegram-desktop neovim git dbeaver elixir nodejs npm
 yay -S gitkraken
 yay -S mailspring
-yay -S spotify
