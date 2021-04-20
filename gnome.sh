408746c7bc25e9f09d0711a0bff65a99bc258570
Mike Kelly <mike@membervault.co>
2021-04-20 09:58:47 -0700

    changes

--- gnome.sh
+++ gnome.sh
@@ -6,14 +6,6 @@ sudo pacman -Syyuu
 sudo pacman -S base-devel xorg gnome gnome-tweaks firefox gdm libappindicator-gtk3 flatpak arc-icon-theme ttf-roboto arc-gtk-theme chrome-gnome-shell
 sudo systemctl enable gdm
 
-# Install yay
-cd /opt/
-sudo git clone https://aur.archlinux.org/yay-git.git
-sudo chown -R mkelly:mkelly ./yay-git
-cd yay-git/
-makepkg -si
-yay -Syyuu
-
 # Install coding and personal apps
 sudo pacman -S code telegram-desktop neovim git dbeaver elixir nodejs npm
 yay -S gitkraken
---
+++ yay.sh
@@ -0,0 +1,7 @@
+# Install yay
+cd /opt/
+sudo git clone https://aur.archlinux.org/yay-git.git
+sudo chown -R mkelly:mkelly ./yay-git
+cd yay-git/
+makepkg -si
+yay -Syyuu
