0bd1deab0a20cde688ca0f11803e719209e3218a
Mike Kelly <mike@membervault.co>
2021-04-27 09:43:27 -0700

    gnome changes

--- gnome.sh
+++ gnome.sh
@@ -3,10 +3,20 @@
 sudo pacman -Syyuu
 
 # Install base gnome stuff
-sudo pacman -S base-devel xorg gnome gnome-tweaks firefox gdm libappindicator-gtk3 flatpak arc-icon-theme ttf-roboto arc-gtk-theme chrome-gnome-shell
+sudo pacman -S xorg
+sudo pacman -S gnome
+sudo pacman -S base-devel gnome-tweaks firefox gdm libappindicator-gtk3 flatpak chrome-gnome-shell
+sudo pacman -S papirus-icon-theme ttf-fira-sans ttf-fira-code arc-gtk-theme
 sudo systemctl enable gdm
 
+# Install yay
+cd /opt/
+sudo git clone https://aur.archlinux.org/yay-git.git
+sudo chown -R $USER:$USER ./yay-git
+cd yay-git/
+makepkg -si
+yay -Syyuu
+
 # Install coding and personal apps
-sudo pacman -S code telegram-desktop neovim git dbeaver elixir nodejs npm
+sudo pacman -S code telegram-desktop neovim git dbeaver elixir nodejs npm geary filezilla
 yay -S gitkraken
-yay -S mailspring
