d00f6f66a7cf070a541a94733c5f4e18761bbc38
Mike Kelly <mike@membervault.co>
2021-05-25 10:37:30 -0700

    wallpapers and tweaks

--- gnome.sh
+++ gnome.sh
@@ -5,8 +5,8 @@ sudo pacman -Syyuu
 # Install base gnome stuff
 sudo pacman -S xorg
 sudo pacman -S gnome
-sudo pacman -S base-devel gnome-tweaks firefox gdm libappindicator-gtk3 flatpak chrome-gnome-shell gnome-disk-utility
-sudo pacman -S papirus-icon-theme ttf-fira-sans ttf-fira-code arc-gtk-theme
+sudo pacman -S base-devel gnome-tweaks firefox gdm libappindicator-gtk3 flatpak chrome-gnome-shell gnome-disk-utility gthumb gnome-calendar
+sudo pacman -S papirus-icon-theme ttf-fira-sans ttf-fira-code arc-gtk-theme lib32-fontconfig ttf-liberation 
 sudo systemctl enable gdm
 
 # Install yay
@@ -18,5 +18,5 @@ makepkg -si
 yay -Syyuu
 
 # Install coding and personal apps
-sudo pacman -S code telegram-desktop neovim git dbeaver elixir nodejs npm geary filezilla
+sudo pacman -S code telegram-desktop neovim git dbeaver elixir nodejs npm geary filezilla discord libreoffice-fresh
 yay -S gitkraken
