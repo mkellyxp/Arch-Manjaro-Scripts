bfcabcbd50849d2970379b44972947b599e123b1
Mike Kelly <mike@membervault.co>
2021-08-10 08:07:43 -0700

    tweak i3 settings

--- i3.sh
+++ i3.sh
@@ -3,9 +3,7 @@
 sudo pacman -Syyuu
 
 # Install base i3 stuff
-sudo pacman -S xorg xorg-xinit i3 dmenu acpilight picom termite
+sudo pacman -S xorg xorg-xinit i3 dmenu acpilight lxappearance arc-gtk-theme base-devel
 
 # Install coding and personal apps
 sudo pacman -S code telegram-desktop neovim git dbeaver
-yay -S gitkraken
-yay -S mailspring
