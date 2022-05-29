17b01df74ca4f8fb4920693e701baf0a4a3beefa
Mike Kelly <mike@membervault.co>
2022-05-29 14:58:29 -0700

    new gnome stuff

--- gnome.sh
+++ gnome.sh
@@ -5,7 +5,7 @@ sudo pacman -Syyuu
 # Install base gnome stuff
 sudo pacman -S xorg xorg-server base-devel
 sudo pacman -S gnome
-sudo pacman -S gnome-tweaks flatpak gthumb vivaldi neovim geary
+sudo pacman -S gnome-tweaks flatpak gthumb neovim docker docker-compose
 sudo pacman -S papirus-icon-theme gnome-themes-extra
 sudo systemctl enable gdm
 
@@ -13,3 +13,5 @@ mkdir ~/.themes
 cp -R /usr/share/themes/Adwaita-dark ~/.themes/
 sudo flatpak override --filesystem=$HOME/.themes
 sudo flatpak override --env=GTK_THEME=Adwaita-dark
+
+#flatpak in /var/lib/flatpak/runtime
