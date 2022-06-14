3770caeab09f7762e5d6da02755b2b71a16a257e
Mike Kelly <mike@membervault.co>
2022-06-14 10:55:41 -0700

    Adding extension AND flatpaks

--- gnome.sh
+++ gnome.sh
@@ -5,7 +5,7 @@ sudo pacman -Syyuu
 # Install base gnome stuff
 sudo pacman -S xorg xorg-server base-devel --noconfirm
 sudo pacman -S gnome --noconfirm
-sudo pacman -S gnome-tweaks flatpak gthumb neovim docker docker-compose nodejs npm --noconfirm
+sudo pacman -S gnome-tweaks gnome-shell-extension-appindicator flatpak gthumb neovim docker docker-compose nodejs npm php7 code --noconfirm
 sudo pacman -S papirus-icon-theme gnome-themes-extra --noconfirm
 sudo systemctl enable gdm
 
@@ -15,5 +15,11 @@ sudo flatpak override --filesystem=$HOME/.themes
 sudo flatpak override --env=GTK_THEME=Adwaita-dark
 
 gsettings set org.gnome.desktop.privacy remember-recent-files false
+
+flatpak install -y com.brave.Browser
+flatpak install -y com.axosoft.GitKraken
+flatpak install -y org.telegram.desktop
+flatpak install -y io.dbeaver.DBeaverCommunity io.dbeaver.DBeaverCommunity.Client.mariadb
+flatpak install -y com.spotify.Client
 
 #flatpak in /var/lib/flatpak/runtime
